c7e35b584358f15f41d5168aa514def582a76d1c
pranav <arora.pranav.2000@gmail.com>
2020-10-30 19:11:58 +0530

    added algo description

--- public/index.js
+++ public/index.js
@@ -6,7 +6,7 @@ import DFS from "./algorithms/dfs.js";
 document.getElementById("modalDispBtn").click();
 
 function InitializeBoard(startNode = null, endNode = null) {
-  const height = window.innerHeight - document.getElementById("nav").offsetHeight - document.getElementById("bullets").offsetHeight; 
+  const height = window.innerHeight - document.getElementById("nav").offsetHeight - document.getElementById("bullets").offsetHeight - document.getElementById("algoDesc").offsetHeight; 
   const board = new Board(window.innerWidth-10, height, 25, 25);
   board.Initialize();
   return board;
@@ -16,14 +16,17 @@ function InitializeBoard(startNode = null, endNode = null) {
 const board = InitializeBoard();
 let algo = null;
 
-window.setAlgo = function(algoNo) {
-  console.log(algoNo);
+window.setAlgo = function(algoNo) { 
+  const desc = document.getElementById("algo-description");
+
   switch(algoNo) {
     case 1:
       algo = new BFS(board);
+      desc.innerHTML = "Breadth-first Search(BFS) is a <b>non-weighted</b> algorithm. It <b>gurantees</b> the shortest path."
       break;
     case 2:
       algo = new DFS(board);
+      desc.innerHTML = "Depth-first Search(DFS) is a <b>non-weighted</b> algorithm. It <b>does not gurantee</b> the shortest path."
       break;
     default:
       algo = new BFS(board);
@@ -40,6 +43,10 @@ function visualize() {
 }
 
 document.getElementById("visualize").addEventListener("click", visualize);
+
+document.getElementById("clear").addEventListener("click", () => {
+  board.clearVisitedAndDistance();
+})
 
 document.getElementById("reset").addEventListener("click", () => {
   board.reInitialize();
